33e79a57af206c031561407462a0d8b389be7849
tony <tonywatkins2014@gmail.com>
2022-03-25 10:42:52 -0400

    index

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,36 @@
+// questions 
 var question = document.querySelector(question)
-//logic
-//variable
-//questions
-//score
+
+
+
+
+
+
+
+
+
+//button function 
+    if ("Incorrect-Answer-is-chosen" )
+    <button onclick="myFunction()"> A </button>
+
+    function myFunction() {
+        document.getElementById(" A ").innerHTML = " Wrong Try Again";
+      
+        console.log ("you are correct!")
+    }
+
+
+
+
+
+// submit  function
+
+
+
+
+
+
+ 
+    
+//add event listener 
+
